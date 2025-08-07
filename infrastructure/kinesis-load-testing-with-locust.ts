f6438294e3e1d2d5e8d62bb294f1dc99303a84f5
Luis Morales <moralesl@users.noreply.github.com>
2025-08-07 12:03:13 +0200

    Add default CW dashboard for load testing
    This shows the most important widgets to observe the stream and the load testing instance. You only have to paste in the instanceId and the streamName

--- infrastructure/kinesis-load-testing-with-locust.ts
+++ infrastructure/kinesis-load-testing-with-locust.ts
@@ -1,5 +1,8 @@
 import * as cdk from "aws-cdk-lib";
 import {
+  CfnDashboard,
+} from "aws-cdk-lib/aws-cloudwatch";
+import {
   AmazonLinuxCpuType,
   CloudFormationInit,
   InitCommand,
@@ -118,11 +121,496 @@ export class KinesisLoadTestingWithLocustStack extends cdk.Stack {
       vpcSubnets: { subnetType: SubnetType.PUBLIC },
       userData: userData,
       init: initData,
+
       detailedMonitoring: true, // Optional, enables detailed monitoring
     });
 
     new cdk.CfnOutput(this, "locust-dashboard-url", {
       value: `http://${locustLoadTestingInstance.instancePublicIp}:${LOCUST_DEFAULT_PORT}`,
+    });
+
+    const dashboard = new CfnDashboard(this, 'KinesisLoadTestDashboard', {
+      dashboardName: 'KinesisLoadTestDashboard',
+      dashboardBody: JSON.stringify({
+        widgets: [
+          {
+            "type": "metric",
+            "x": 0,
+            "y": 0,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                ["AWS/EC2", "CPUUtilization", "InstanceId", "${InstanceId}", { "label": "${InstanceId} (TemperatureSensorDataProducer)", "region": `${this.region}` }],
+                ["...", { "stat": "Average", "region": `${this.region}` }]
+              ],
+              "view": "timeSeries",
+              "stat": "Maximum",
+              "period": 60,
+              "stacked": false,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "region": `${this.region}`,
+              "title": "CPU utilization (%)"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 6,
+            "y": 0,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                ["AWS/EC2", "StatusCheckFailed", "InstanceId", "${InstanceId}", { "label": "${InstanceId} (TemperatureSensorDataProducer)" }]
+              ],
+              "view": "timeSeries",
+              "stat": "Average",
+              "period": 60,
+              "stacked": false,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "region": `${this.region}`,
+              "title": "Status check failed (any) (count)"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 12,
+            "y": 0,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                ["AWS/EC2", "StatusCheckFailed_Instance", "InstanceId", "${InstanceId}", { "label": "${InstanceId} (TemperatureSensorDataProducer)" }]
+              ],
+              "view": "timeSeries",
+              "stat": "Average",
+              "period": 60,
+              "stacked": false,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "region": `${this.region}`,
+              "title": "Status check failed (instance) (count)"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 18,
+            "y": 0,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "view": "timeSeries",
+              "stat": "Average",
+              "period": 60,
+              "stacked": false,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "region": `${this.region}`,
+              "metrics": [
+                ["AWS/EC2", "StatusCheckFailed_System", "InstanceId", "${InstanceId}", { "label": "${InstanceId} (TemperatureSensorDataProducer)" }]
+              ],
+              "title": "Status check failed (system) (count)"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 0,
+            "y": 4,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "view": "timeSeries",
+              "stat": "Average",
+              "period": 60,
+              "stacked": false,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "region": `${this.region}`,
+              "metrics": [
+                ["AWS/EC2", "NetworkIn", "InstanceId", "${InstanceId}", { "label": "${InstanceId} (TemperatureSensorDataProducer)" }]
+              ],
+              "title": "Network in (bytes)"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 6,
+            "y": 4,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "view": "timeSeries",
+              "stat": "Average",
+              "period": 60,
+              "stacked": false,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "region": `${this.region}`,
+              "metrics": [
+                ["AWS/EC2", "NetworkOut", "InstanceId", "${InstanceId}", { "label": "${InstanceId} (TemperatureSensorDataProducer)" }]
+              ],
+              "title": "Network out (bytes)"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 12,
+            "y": 4,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "view": "timeSeries",
+              "stat": "Average",
+              "period": 60,
+              "stacked": false,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "region": `${this.region}`,
+              "metrics": [
+                ["AWS/EC2", "NetworkPacketsIn", "InstanceId", "${InstanceId}", { "label": "${InstanceId} (TemperatureSensorDataProducer)" }]
+              ],
+              "title": "Network packets in (count)"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 18,
+            "y": 4,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "view": "timeSeries",
+              "stat": "Average",
+              "period": 60,
+              "stacked": false,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "region": `${this.region}`,
+              "metrics": [
+                ["AWS/EC2", "NetworkPacketsOut", "InstanceId", "${InstanceId}", { "label": "${InstanceId} (TemperatureSensorDataProducer)" }]
+              ],
+              "title": "Network packets out (count)"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 6,
+            "y": 8,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                [{ "expression": `${SHARD_LIMIT} * 1 * IF(m5, 1, 1)`, "label": "Incoming data Limit", "id": "e6", "color": "#d62728", "region": `${this.region}`, "period": 60 }],
+                [{ "expression": "m5/1024/1024/PERIOD(m5)", "id": "e1", "label": "Incoming data - sum (MB/s)", "region": `${this.region}`, "period": 60 }],
+                ["AWS/Kinesis", "IncomingBytes", "StreamName", "${StreamName}", { "id": "m5", "visible": false, "stat": "Sum" }]
+              ],
+              "region": `${this.region}`,
+              "yAxis": {
+                "left": {
+                  "min": 0,
+                  "showUnits": false
+                }
+              },
+              "title": "Incoming data - sum (MB/s)",
+              "period": 60,
+              "view": "timeSeries",
+              "stacked": false,
+              "stat": "Average"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 0,
+            "y": 8,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                [{ "expression": `${SHARD_LIMIT} *\n              1000 * PERIOD(m6) * IF(m6, 1, 1)`, "label": "Incoming records Limit", "id": "e6", "color": "#d62728", "period": 60, "region": `${this.region}` }],
+                ["AWS/Kinesis", "IncomingRecords", "StreamName", "${StreamName}", { "id": "m6", "visible": true }]
+              ],
+              "region": `${this.region}`,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "stat": "Sum",
+              "title": "Incoming data - sum (Count)",
+              "period": 60,
+              "view": "timeSeries",
+              "stacked": false
+            }
+          },
+          {
+            "type": "metric",
+            "x": 12,
+            "y": 8,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                [{ "expression": "m7/1024/1024/PERIOD(m7)", "id": "e1", "label": "PutRecord - sum (MB/s)", "region": `${this.region}`, "period": 60 }],
+                ["AWS/Kinesis", "PutRecord.Bytes", "StreamName", "${StreamName}", { "id": "m7", "visible": false, "stat": "Sum" }]
+              ],
+              "region": `${this.region}`,
+              "yAxis": {
+                "left": {
+                  "min": 0,
+                  "showUnits": false
+                }
+              },
+              "title": "PutRecord - sum (MB/s)",
+              "period": 60,
+              "view": "timeSeries",
+              "stacked": false,
+              "stat": "Average"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 18,
+            "y": 8,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                ["AWS/Kinesis", "PutRecord.Latency", "StreamName", "${StreamName}", { "id": "m8", "visible": true }]
+              ],
+              "region": `${this.region}`,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "stat": "Average",
+              "title": "PutRecord latency - average (Milliseconds)",
+              "period": 60,
+              "view": "timeSeries",
+              "stacked": false
+            }
+          },
+          {
+            "type": "metric",
+            "x": 6,
+            "y": 12,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                ["AWS/Kinesis", "PutRecord.Success", "StreamName", "${StreamName}", { "id": "m9", "visible": true }]
+              ],
+              "region": `${this.region}`,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "stat": "Average",
+              "title": "PutRecord success - average (Percent)",
+              "period": 60,
+              "view": "timeSeries",
+              "stacked": false
+            }
+          },
+          {
+            "type": "metric",
+            "x": 0,
+            "y": 12,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                [{ "expression": "m10/1024/1024/PERIOD(m10)", "id": "e2", "label": "PutRecords - sum (MB/s)", "region": `${this.region}`, "period": 60 }],
+                ["AWS/Kinesis", "PutRecords.Bytes", "StreamName", "${StreamName}", { "id": "m10", "visible": false, "stat": "Sum" }]
+              ],
+              "region": `${this.region}`,
+              "yAxis": {
+                "left": {
+                  "min": 0,
+                  "showUnits": false
+                }
+              },
+              "title": "PutRecords - sum (MB/s)",
+              "period": 60,
+              "view": "timeSeries",
+              "stacked": false,
+              "stat": "Average"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 12,
+            "y": 12,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                ["AWS/Kinesis", "PutRecords.Latency", "StreamName", "${StreamName}", { "id": "m11", "visible": true }]
+              ],
+              "region": `${this.region}`,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "stat": "Average",
+              "title": "PutRecords latency - average (Milliseconds)",
+              "period": 60,
+              "view": "timeSeries",
+              "stacked": false
+            }
+          },
+          {
+            "type": "metric",
+            "x": 12,
+            "y": 16,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                ["AWS/Kinesis", "WriteProvisionedThroughputExceeded", "StreamName", "${StreamName}", { "id": "m13", "visible": true }]
+              ],
+              "region": `${this.region}`,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "stat": "Average",
+              "title": "Write throughput exceeded - average (Count)",
+              "period": 60,
+              "view": "timeSeries",
+              "stacked": false
+            }
+          },
+          {
+            "type": "metric",
+            "x": 0,
+            "y": 16,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                [{ "expression": "(m11/m12) * 100", "id": "e3", "label": "PutRecords successful records - average (Percent)", "region": `${this.region}`, "period": 60 }],
+                ["AWS/Kinesis", "PutRecords.SuccessfulRecords", "StreamName", "${StreamName}", { "id": "m11", "visible": false }],
+                [".", "PutRecords.TotalRecords", ".", ".", { "id": "m12", "visible": false }]
+              ],
+              "region": `${this.region}`,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "title": "PutRecords successful records - average (Percent)",
+              "period": 60,
+              "view": "timeSeries",
+              "stacked": false,
+              "stat": "Sum"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 6,
+            "y": 16,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                [{ "expression": "(m13/m12) * 100", "id": "e4", "label": "PutRecords failed records - average (Percent)", "region": `${this.region}`, "period": 60 }],
+                ["AWS/Kinesis", "PutRecords.FailedRecords", "StreamName", "${StreamName}", { "id": "m13", "visible": false }],
+                [".", "PutRecords.TotalRecords", ".", ".", { "id": "m12", "visible": false }]
+              ],
+              "region": `${this.region}`,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "title": "PutRecords failed records - average (Percent)",
+              "period": 60,
+              "view": "timeSeries",
+              "stacked": false,
+              "stat": "Sum"
+            }
+          },
+          {
+            "type": "metric",
+            "x": 18,
+            "y": 12,
+            "width": 6,
+            "height": 4,
+            "properties": {
+              "metrics": [
+                [{ "expression": "(m15/m12) * 100", "id": "e5", "label": "PutRecords throttled records - average (Percent)", "region": `${this.region}`, "period": 60 }],
+                ["AWS/Kinesis", "PutRecords.ThrottledRecords", "StreamName", "${StreamName}", { "id": "m15", "visible": false }],
+                [".", "PutRecords.TotalRecords", ".", ".", { "id": "m12", "visible": false }]
+              ],
+              "region": `${this.region}`,
+              "yAxis": {
+                "left": {
+                  "min": 0
+                }
+              },
+              "title": "PutRecords throttled records - average (Percent)",
+              "period": 60,
+              "view": "timeSeries",
+              "stacked": false,
+              "stat": "Sum"
+            }
+          }
+        ],
+        variables: [
+          {
+            type: "property",
+            property: "InstanceId",
+            inputType: "input",
+            id: "InstanceId",
+            label: "InstanceId",
+            visible: true
+          },
+          {
+            type: "property",
+            property: "StreamName",
+            inputType: "input",
+            id: "StreamName",
+            label: "StreamName",
+            visible: true
+          }
+        ]
+      })
+    });
+
+    // Output the dashboard URL
+    new cdk.CfnOutput(this, 'DashboardUrl', {
+      value: `https://${this.region}.console.aws.amazon.com/cloudwatch/home?region=${this.region}#dashboards:name=${dashboard.dashboardName}`,
+      description: 'URL for the CloudWatch Dashboard',
     });
   }
 }
